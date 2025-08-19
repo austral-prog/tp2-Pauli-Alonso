791702ba9b8c51a08728b309b45afeb07ce08433
Paulina Alonso <alonsopaulina32@gmail.com>
2025-08-18 21:01:13 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,3 +1,20 @@
 def change():
     expense = 23.75
-    money = 100
+    money = 100  
+    vuelto = money-expense
+    centavos = (vuelto-int(vuelto))*100 print
+
+    print("Ingresar gasto:")  
+    print(expense) 
+    print("Dinero recibido")
+    print(str(money) + "\n") 
+ 
+    print("Vuelto" + "\n")
+    
+    print("Pesos:")
+    print(int(vuelto))
+    print("Centavos:")
+    print(int(centavos))
+change() 
+ 
+
